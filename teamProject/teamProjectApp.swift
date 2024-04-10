623959ce1d30fbcf42107415ceedd3d0a59a8aa2
ah0556702 <ah0556702@otc.edu>
2024-04-10 09:27:36 -0500

    Initial Commit

---
+++ teamProject/teamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamProjectApp.swift
+//  teamProject
+//
+//  Created by HARMON, AUDREY on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct teamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
