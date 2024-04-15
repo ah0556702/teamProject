b217940a95b238573d4c01abed7853da4a186dd1
Louis Lathrop <ll0128774@otc.edu>
2024-04-15 09:46:44 -0500

    updated API URL

--- teamProject/ContentView.swift
+++ teamProject/ContentView.swift
@@ -17,7 +17,7 @@ struct Notice: Codable {
 }
 
 struct Result: Codable{
-    var notices: [Notice]
+    var items: [Notice]
 }
 
 struct ContentView: View {
@@ -31,28 +31,28 @@ struct ContentView: View {
     
     // fetches the Users from the github api
     
-//    func getUsers(){ // function for searching based on user input
-//        // Add Search content
-//        let trimmedSearchText = searchText.trimmingCharacters(in: .whitespacesAndNewlines) // take whitespace out of input text
-//        
-//        //Proceed only if searchText is not empty or just whitespace
-//        guard !trimmedSearchText.isEmpty else {
-//            return
-//        }
-//        if let apiURL = URL(string:"https://api.github.com/search/users?q=\(trimmedSearchText)"){ // search the api data
-//            var request = URLRequest(url:apiURL) // store the request response
-//            request.httpMethod = "GET" // grab the request response
-//            URLSession.shared.dataTask(with: request){ // stores data in memory using session
-//                data, response,error in // grab data from response and process error
-//                if let userData = data {
-//                    if let usersFromAPI = try? JSONDecoder().decode(Result.self, from: userData){
-//                        users = usersFromAPI.items
-//                        print(users)
-//                    }
-//                }
-//            }.resume()
-//        }
-//    }
+    func getUsers(){ // function for searching based on user input
+        // Add Search content
+        let trimmedSearchText = searchText.trimmingCharacters(in: .whitespacesAndNewlines) // take whitespace out of input text
+        
+        //Proceed only if searchText is not empty or just whitespace
+        guard !trimmedSearchText.isEmpty else {
+            return
+        }
+        if let apiURL = URL(string:"https://ws-public.interpol.int/notices/v1/red?nationality=US&page=1&resultPerPage=5"){ // search the api data
+            var request = URLRequest(url:apiURL) // store the request response
+            request.httpMethod = "GET" // grab the request response
+            URLSession.shared.dataTask(with: request){ // stores data in memory using session
+                data, response,error in // grab data from response and process error
+                if let userData = data {
+                    if let usersFromAPI = try? JSONDecoder().decode(Result.self, from: userData){
+                        notices = usersFromAPI.items
+                        print(notices)
+                    }
+                }
+            }.resume()
+        }
+    }
 }
 
 #Preview {
